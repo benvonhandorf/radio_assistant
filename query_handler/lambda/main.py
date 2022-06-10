7b92b90418867d7680e6bbbd4ca5138f42b96e25
Ben Von Handorf <ben@skyironstudio.com>
2022-06-10 08:30:43 -0400

    Iterating

--- query_handler/lambda/main.py
+++ query_handler/lambda/main.py
@@ -21,71 +21,104 @@ def get_band_status_info():
     return band_status_info
 
 def main(event, context):
-    print(event)
+    try:
+        print(event)
 
-    print(context)
+        print(context)
 
-    request = json.loads(event['body'])
+        request = json.loads(event['body'])
 
-    session_path = parse('$..session')
+        session_path = parse('$..session.id')
 
-    session_id = session_path.find(request)[0].value
+        session_id = session_path.find(request)[0].value
 
-    intent_path = parse('$..intent.name')
-    
-    intent = intent_path.find(request)[0].value
+        intent_path = parse('$..intent.name')
+        
+        intent = intent_path.find(request)[0].value
 
-    response_speech = ""
+        print(f'Intent: {intent}')
 
-    band_status_info = get_band_status_info()
+        response_speech = ""
 
-    if intent == "band_selection":
-        band_string = parse('intent.params.band.resolved').find(request)[0].value
+        band_status_info = get_band_status_info()
 
-        band_results = parse(f"bands[?(band=='{band_string}')]").find(band_status_info)
+        print(f'Retrieved band status:{band_status_info}')
 
-        if band_results:
-            band = band_results[0].value
+        if intent == "band_selection":
+            print(f'Band selection')
 
-            response_speech = f'{band["band"]} is currently {band["status"]}.'
-        else:
-            response_speech = f'I don\'t have any information for {band_string}.'
+            band_string = parse('$..intent.params.band.resolved').find(request)[0].value
+
+            print(f'Band selection: {band_string}')
+
+            # ?(@.band=='{band_string}')
+
+            # band_path = f"$..bands[0]"
+
+            # print(f'Band string: {band_path}')
+
+            # parsed_path = parse(band_path)
+
+            # print(f'Parsed path: {parsed_path}')
+
+            # band_results = parsed_path.find(band_status_info)
+
+            band_results = [{"value": band_status_info["bands"][0]}]
+
+            print(f'Band Results: {band_results}')
+
+            if band_results:
+                print(f'Found band results')
+
+                band = band_results[0]['value']
+
+                print(f'Band Info: {band}')
+
+                response_speech = f'{band["band"]} is currently {band["status"]}.'
+            else:
+                response_speech = f'I don\'t have any information for {band_string}.'
 
 
-    elif intent == "all_band_status":
-        band_results = parse(f'bands[*]').find(band_status_info)
+        elif intent == "all_band_status":
+            band_results = parse(f'bands[*]').find(band_status_info)
 
-        if band_results:
-            band_summary = '  '.join([f'{band.value["band"]} is {band.value["status"]}.' for band in band_results])
+            if band_results:
+                band_summary = '  '.join([f'{band.value["band"]} is {band.value["status"]}.' for band in band_results])
 
-            response_speech = f'Right now, {band_summary}'
-        else:
-            response_speech = f'I\'m afraid I can\'t tell right now.'
+                response_speech = f'Right now, {band_summary}'
+            else:
+                response_speech = f'I\'m afraid I can\'t tell right now.'
 
-    result = {
-        'session': session_id,
-        'prompt': {
-            'firstSimple': {
-                 'speech': response_speech
-            }
-        },
-        'scene': {
-            'next': {
-                'name': 'PromptForBand'
+        print(f'Response: {response_speech} {session_id}')
+
+        result = {
+            'session': session_id,
+            'prompt': {
+                'firstSimple': {
+                    'speech': response_speech
+                }
+            },
+            'scene': {
+                'next': {
+                    'name': 'PromptForBand'
+                }
             }
         }
-    }
 
-    return {
-        'statusCode': 200,
-        'headers': {
-            'Content-Type': 'application/json'
-        },
-        'body': json.dumps(result)
-    }
+        print(f'Result: {result}')
+
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Content-Type': 'application/json'
+            },
+            'body': json.dumps(result)
+        }
+    except Exception as exception:
+        print(f'Unable to process request: {exception}')
 
 if __name__ == "__main__":    
-    with open('sample_request_all.json', 'r') as sample_file:
+    with open('sample_request.json', 'r') as sample_file:
         input_json = json.load(sample_file)
 
     test_context = {}
