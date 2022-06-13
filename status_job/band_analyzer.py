ed7f21bc8d05ec7f7ac1dbd28d7aa8fda3b438ee
Ben Von Handorf <ben@skyironstudio.com>
2022-06-12 22:57:42 -0400

    Plotly image generation

--- status_job/band_analyzer.py
+++ status_job/band_analyzer.py
@@ -1,10 +1,61 @@
 from influxdb_client import InfluxDBClient, Point
+import logging
+from datetime import datetime, timedelta
+import pytz
 
 class BandAnalyzer():
-    def __init__(self, client):
-        self.client = client
+    def __init__(self, query_api):
+        self.query_api = query_api
 
-    def list_bands(self, timespan_minutes):
+    def construct_start_stop_utc(self, evaluation_time, timespan_minutes):
+        time_delta = timedelta(minutes=timespan_minutes)
+
+        start_utc = pytz.utc.localize(evaluation_time - time_delta)
+        end_utc = pytz.utc.localize(evaluation_time)
+
+        return (start_utc, end_utc)
+
+    def list_bands(self, evaluation_time=datetime.now(), timespan_minutes=60):
         """Returns a list of the bands seen in the last `timespan_minutes` minutes."""
-        pass
+        
+        start_utc, stop_utc = self.construct_start_stop_utc(evaluation_time, timespan_minutes)
+
+        query = f"""from(bucket: "radio/autogen")
+  |> range(start: {start_utc.isoformat()} , stop: {stop_utc.isoformat()})
+  |> filter(fn: (r) => r["_measurement"] == "spot")
+  |> keep(columns: ["band"])
+  |> group(columns: ["band"])
+  |> distinct()
+  |> keep(columns: ["band"])"""
+
+        logging.debug(query)
+
+        results = self.query_api.query_data_frame(query)
+
+        return results['band'].tolist()
+
+
+
+    def retrieve_band_polar_data(self, band_name, evaluation_time=datetime.now(), timespan_minutes=60):
+        start_utc, stop_utc = self.construct_start_stop_utc(evaluation_time, timespan_minutes)
+
+        query = f"""from(bucket: "radio/autogen")
+  |> range(start: {start_utc.isoformat()} , stop: {stop_utc.isoformat()})
+  |> filter(fn: (r) => r["_measurement"] == "spot")
+  |> filter(fn: (r) => r["power"] == "5.0")
+  |> filter(fn: (r) => r["band"] == "{band_name}")
+  |> filter(fn: (r) => r["_field"] == "azimuth")
+  |> map(fn: (r) => ({{ r with segment: (r._value + 22.5 - ((r._value + 22.5) % 45.0)) % 360.0 }}))
+  |> group(columns: ["segment"])
+  |> count()
+  |> group()"""
+
+        logging.debug(query)
+
+        results = self.query_api.query_data_frame(query)
+
+        return results
+
+
+
 
--- status_job/contruct_band_status_report.py
+++ status_job/contruct_band_status_report.py
@@ -1,7 +1,21 @@
 import os
-import boto3
+
 import argparse
+
+import boto3
 from botocore.exceptions import ClientError
+
+from band_analyzer import BandAnalyzer
+
+from influxdb_client import InfluxDBClient, Point
+
+import logging
+
+from datetime import datetime
+
+import plotly.graph_objects as go
+
+import plotly.express as px
 
 # session = boto3.Session(profile_name='radio_assistant_writer')
 # s3_client = session.client('s3')
@@ -11,15 +25,57 @@ from botocore.exceptions import ClientError
 # except ClientError as e:
 #     print(e)
 
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
 if __name__ == "__main__":
     argument_parser = argparse.ArgumentParser()
-    argument_parser.add_argument('--influx_server', default="littlerascal.local",
+    argument_parser.add_argument('--influx_server', default="http://littlerascal.local:8086",
         help="InfluxDB Server")
     argument_parser.add_argument('--influx_token', default=os.environ['INFLUX_TOKEN'],
         help="InfluxDB Auth Token")
+    argument_parser.add_argument('--influx_org', default='skyiron',
+        help="InfluxDB Organization")
     argument_parser.add_argument('--aws_profile', default='radio_assistant_writer',
         help="AWS Profile")
 
     args = argument_parser.parse_args()
 
+    influx_client = InfluxDBClient(url=args.influx_server, token=args.influx_token, org=args.influx_org)
+
+    query_api = influx_client.query_api()
+
+    band_analyzer = BandAnalyzer(query_api)
+
+    bands = band_analyzer.list_bands()
+
+    logging.info(f'Bands Found: {bands}')
+
+    # figure = go.Figure()
+
+    for band in bands:
+        logging.info(f'Retrieving band {band}')
+
+        band_polar_data = band_analyzer.retrieve_band_polar_data(band)
+
+        logging.debug(band_polar_data)
+
+        # figure.add_trace(go.Barpolar(band_polar_data,
+        #     name = band,
+        #     theta = "segment",
+        #     r = "_value"
+        # ))
+
+        figure = px.bar_polar(band_polar_data,
+            r="_value", 
+            theta="segment",
+            template="plotly_dark",
+            color_discrete_sequence= px.colors.sequential.Plasma_r)
+
+        break
+
+    if not os.path.exists("images"):
+        os.mkdir("images")
+
+    figure.write_image("images/output.png")
     
